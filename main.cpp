fe7506b3af42fae972cd49c42325c22834e16c7b
Chuy <jro867@gmail.com>
2014-04-05 20:35:12 -0600

    work..

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: nuxer
+ *
+ * Created on April 5, 2014, 8:22 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "work stupid thing";
+    return 0;
+}
+
